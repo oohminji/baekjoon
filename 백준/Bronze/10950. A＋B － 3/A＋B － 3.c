1e7fdc02a376ef0caa98da1e90df9ae4f6e6c647
오민지 <157579432+oohminji@users.noreply.github.com>
2025-05-26 21:29:33 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -4,13 +4,11 @@ int main(void)
 {
     int a,b;
     int n;
-    int sum =0;
     scanf("%d",&n);
     for(int i =0; i<n; i++)
     {
        scanf("%d %d", &a, &b);
-        sum = a+b;
-       printf("%d\n",sum);
+       printf("%d\n",a+b);
     }
     return 0;
 }
